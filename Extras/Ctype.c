c84a8456a4d82025376167983c8451a33fd7456a
LuizR92 <lzricardoliver@gmail.com>
2026-04-04 19:38:24 -0300

    Adiciona verificação de letra e número

--- Extras/Ctype.c
+++ Extras/Ctype.c
@@ -1,1 +1,49 @@
+#include <stdio.h>
+#include <ctype.h>
 
+    int main () {
+    char c;
+
+        printf("Digite uma Letra ou Número: ");
+        scanf(" %c", &c);
+
+        if(isalpha (c)) {
+            printf("É uma letra ");
+        
+            if(isupper (c)) {
+                printf("maiúscula!\n");
+            }
+
+            else if(islower (c)) {
+                printf("minúscula!\n");
+            }
+
+        }
+        
+        else if (isdigit (c)) {
+            printf("É um número!\n");
+        }
+
+        else {
+            printf("É outra coisa!\n");
+        }
+
+        return 0;
+
+    }
+
+
+        
+        
+
+
+
+
+
+
+
+
+
+
+    return 0;    
+}
