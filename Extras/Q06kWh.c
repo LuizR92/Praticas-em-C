83e1e2ef6698ef72804e34cd5fe7f71cc3ed76ec
LuizR92 <lzricardoliver@gmail.com>
2026-04-10 18:12:07 -0300

    melhorando

--- Extras/Q06kWh.c
+++ Extras/Q06kWh.c
@@ -1,89 +1,42 @@
-/*
-
-Para resolver essa situação faça essas 3 perguntas:
-
-1 - Quais dados eu tenho?
---> Consumo
---> Tipo
-
-2 - O que preciso descobrir (exercício pede) ?
---> O preco
-
-3. De que depende a resposta (como eu descubro o preço)
-O preço depende de:
--> tipo da instalação
--> faixa de consumo
-
-
--->>>>>> Resumindo o Raciocínio <<<<<<--
-
----> primeiro descubro o tipo
----> depois descubro a faixa
----> depois calculo o preço<
-
-*/
-
-//===========================================================================================================================//
-
 #include <stdio.h>
-int main()
-{
 
-    float consumo; // Quantidade de kWh consumida.
-    char tipo;     // tipo de instação : R = Residencial C = Comercial I = Industrial.
-    float preco;   // Valor final.
-    
-    printf("Digite o consumo em kWh: ");
-    scanf("%f", &consumo);
-    
-    
-    printf("Digite o tipo de instacao (R = Residencial C = Comercial I = Industrial): ");
+int main() {
+    char tipo;
+    float kWh;
+    float preco;
+    float total;
+
+    printf("Para calcular o custo pelo fornecimento de energia, primeiramente digite o tipo de instalacao: (R= Residencial / C= Comercial / I = Industrial) = ");
     scanf(" %c", &tipo);
 
-    if (tipo == 'R' || tipo == 'r')
-    {
-        if (consumo <= 500)
-        {
-            preco = consumo * 0.40;
-        }
-        else
-        {
-            preco = consumo * 0.65;
-        }
-    }
+    printf("\nAgora digite o consumo em kWh: ");
+    scanf("%f", &kWh);
 
-    else if (tipo == 'C' || tipo == 'c')
-    {
-
-        if (consumo <= 1000)
-        {
-            preco = consumo * 0.55;
+    if (tipo == 'R' || tipo == 'r') {
+        if (kWh <= 500) {
+            preco = 0.40;
+        } else {
+            preco = 0.65;
         }
-        else
-        {
-            preco = consumo * 0.60;
+    } else if (tipo == 'C' || tipo == 'c') {
+        if (kWh <= 1000) {
+            preco = 0.55;
+        } else {
+            preco = 0.60;
         }
-    }
-
-    else if (tipo == 'I' || tipo == 'i')
-    {
-        if (consumo <= 5000)
-        {
-            preco = consumo * 0.55;
+    } else if (tipo == 'I' || tipo == 'i') {
+        if (kWh <= 5000) {
+            preco = 0.55;
+        } else {
+            preco = 0.60;
         }
-        else
-        {
-            preco = consumo * 0.60;
-        }
-    }
-
-    else
-    {
+    } else {
         printf("Tipo Invalido!\n");
         return 1;
     }
 
-    printf("Valor a pagar: R$ %.2f\n", preco);
+    total = kWh * preco;
+    printf("Valor total a pagar: R$ %.2f\n", total);
 
     return 0;
 }
