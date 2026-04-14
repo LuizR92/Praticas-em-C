dd9e547f99ff76cd449e9d409242c0c1e4df84b9
LuizR92 <lzricardoliver@gmail.com>
2026-04-14 09:01:30 -0300

    commit

--- Lista-2/Q09.c
+++ Lista-2/Q09.c
@@ -25,35 +25,34 @@ Compra     Consegue empréstimo?
 */
 
 #include <stdio.h>
-int main (){
+int main()
+{
     char dinheiro;
     char emprestimo;
-    
+
     printf("Você tem dinheiro? (S = SIM / N = Não): ");
     scanf(" %c", &dinheiro);
-    
-    if (dinheiro == 'S' || dinheiro == 's') {
+
+    if (dinheiro == 'S' || dinheiro == 's')
+    {
         printf("Pode Comprar!\n");
-        }
-        
-    else {
+    }
+
+    else
+    {
         printf("\nConsegue Emprestimo? (S == SIM / N == Não): ");
         scanf(" %c", &emprestimo);
-        
-        if (emprestimo == 'S' || emprestimo == 's') {
+
+        if (emprestimo == 'S' || emprestimo == 's')
+        {
             printf("Pode Comprar!\n");
         }
-        
-        else {
+
+        else
+        {
             printf("Não pode comprar!\n");
         }
-        
-        
     }
-    
+
     return 0;
-        
 }
-
-
-
