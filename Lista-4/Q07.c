6350c2fab4689a9ac67bf937fadd2b16b35c5479
LuizR92 <lzricardoliver@gmail.com>
2026-05-04 11:44:17 -0300

    finalizando

--- Lista-4/Q07.c
+++ Lista-4/Q07.c
@@ -1,1 +1,25 @@
-// fazer em casa.
+//Leia números até a soma ultrapassar 100.
+
+#include <stdio.h>
+
+int main() {
+    int numero;
+    int soma = 0;
+
+    printf("Digite um numero: ");
+    scanf("%d", &numero);
+
+    while (soma <= 100) {
+        soma = soma + numero;
+
+        
+        if (soma <= 100) {
+            printf("A soma ainda esta em %d. Digite outro: ", soma);
+            scanf("%d", &numero);
+        }
+        
+    }
+        printf("\nPronto! A soma final foi %d e ultrapassou 100.\n", soma);
+
+return 0;
+}
