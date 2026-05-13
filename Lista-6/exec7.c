dcc6e1aec9702ca313e3a4b3c9c8559ad472f1eb
LuizR92 <lzricardoliver@gmail.com>
2026-05-13 20:43:51 -0300

    adicionando exercício 7.

---
+++ Lista-6/exec7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+
+int numeros[8];
+int pos;
+
+for(pos = 0; pos < 8; pos++) {
+    
+    printf("Digite o %dº número: ", pos+1);
+    scanf("%d", &numeros[pos]);
+}
+printf("\n");
+for(pos = 7; pos >= 0; pos--) {
+    printf("%dº número %d\n", pos+1, numeros[pos]);
+}
+
+return 0;
+}
